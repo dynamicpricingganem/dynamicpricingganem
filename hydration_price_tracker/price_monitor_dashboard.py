4a13669915988e7acde0f716e10218cc03d3a497
dynamicpricingganem <rodrigo.deleon@ganem.mx>
2025-05-21 20:08:37 -0500

    Update price_monitor_dashboard.py

--- hydration_price_tracker/price_monitor_dashboard.py
+++ hydration_price_tracker/price_monitor_dashboard.py
@@ -76,7 +76,6 @@ if os.path.exists(DATA_FILE):
             with col1:
                 st.image(row["img_url"], width=150)
             with col2:
-        with col2:
             st.markdown(f"**{row['product']}**  \nRetailer: {row['retailer']}  \nPrice: {row['promo']}")
             st.markdown(f"**{row['product']}**  \nRetailer: {row['retailer']}  \nPrice: {row['promo']}")
 else:
